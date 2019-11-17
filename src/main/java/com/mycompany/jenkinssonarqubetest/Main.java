aa6cf4984599a6067bab314068b8dbb722bd172d
Jose Pablo Ramirez Mayorga <jota_ramirezm@outlook.com>
2019-11-16 20:30:22 -0600

    SecondCommit

---
+++ src/main/java/com/mycompany/jenkinssonarqubetest/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jenkinssonarqubetest;
+
+/**
+ *
+ * @author josepabloramirez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins, I'am going to be scanned by sonarqube");
+    }
+    
+}
